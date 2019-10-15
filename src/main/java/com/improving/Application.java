783dbc92861f3dc71b852dda8957ad89b0e115c1
Paloma Pineda <Paloma.Pineda@improving.com>
2019-10-15 13:27:00 -0500

    Yo....

--- src/main/java/com/improving/Application.java
+++ src/main/java/com/improving/Application.java
@@ -7,4 +7,5 @@ public class Application {
     }
     // I'm on Siri's team but I don't pull often enough
     // so I'm going to run into some pain...
+    //have ya'll tried these KIND protein bars?
 }
