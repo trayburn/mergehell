f52d7a7638d0de3e6141cbb55889f7ab493c3bc1
Paloma Pineda <Paloma.Pineda@improving.com>
2019-10-15 13:24:30 -0500

    Yo....

--- src/main/java/com/improving/Application.java
+++ src/main/java/com/improving/Application.java
@@ -1,6 +1,10 @@
 package com.improving;
 
 public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("... because ninjas....");
+    }
     // I'm on Siri's team but I don't pull often enough
     // so I'm going to run into some pain...
 }
