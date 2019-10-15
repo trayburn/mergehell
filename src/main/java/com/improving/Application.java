f45948137da0ac3776ac35395e0164a092928d14
Siri Bafna <Siri.Bafna@improving.com>
2019-10-15 13:32:36 -0500

    Siriz Commit #2

--- src/main/java/com/improving/Application.java
+++ src/main/java/com/improving/Application.java
@@ -12,9 +12,5 @@ public class Application {
         System.out.println("Hello World");
         System.out.println("... because ninjas....");
     }
-    // I'm on Siri's team but I don't pull often enough
-    // so I'm going to run into some pain...
-
-    // CHANGE ONE
-    //CHANGE 2
+    // hello
 }
