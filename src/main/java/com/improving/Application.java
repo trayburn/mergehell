832f547bf4f7c70f610700697ea954ca5b666c6f
Emily Stacy <Emily.Stacy@improving.com>
2019-10-15 13:24:32 -0500

    hello from Emily

--- src/main/java/com/improving/Application.java
+++ src/main/java/com/improving/Application.java
@@ -3,4 +3,5 @@ package com.improving;
 public class Application {
     // I'm on Siri's team but I don't pull often enough
     // so I'm going to run into some pain...
+    // hi this is the new message
 }
